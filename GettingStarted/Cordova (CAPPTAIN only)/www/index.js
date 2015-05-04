0e569d5a6aa643da31b80a0663bb0b24f2bb71e4
Olivier Goguel <o@goguel.com>
2015-05-04 23:19:35 +0200

    Updated licence

--- GettingStarted/Cordova (CAPPTAIN only)/www/index.js
+++ GettingStarted/Cordova (CAPPTAIN only)/www/index.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014,2015 Oliver Goguel - o@goguel.com
+ * Copyright (c) Microsoft Corporation.  All rights reserved.
  * Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
  */
 
