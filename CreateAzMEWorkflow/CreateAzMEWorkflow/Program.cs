6961f8f8a12d53bc301edf46ea519fdc1787c693
Piyush Joshi <piyushjo@microsoft.com>
2015-05-12 16:17:06 -0700

    New sample for AzME backend API integration

---
+++ CreateAzMEWorkflow/CreateAzMEWorkflow/Program.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.SharePoint.Client;
+using MSDN.Samples.ClaimsAuth;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CreateAzMEWorkflow
+{
+    class Program
+    {
+        const string url = "";
+        const string ApplicationId = "";
+        const string ApiKey = "";
+        const string targetSharepointSite = "";
+        const string targetSharepointList = "";
+        
+        [STAThread]
+        static void Main(string[] args)
+        {
+            CreateCampaignFromSharepoint();
+            Console.Read();
+        }
+
+        static async void CreateCampaignFromSharepoint()
+        {
+            using (ClientContext clientContext = ClaimClientContext.GetAuthenticatedContext(targetSharepointSite))
+            {
+                // Using https://code.msdn.microsoft.com/Remote-Authentication-in-b7b6f43c for authentication     
+                Web site = clientContext.Web;
+                List list = site.Lists.GetByTitle(targetSharepointList);
+                CamlQuery query = new CamlQuery();
+                query.ViewXml = "<View/>";
+                ListItemCollection items = list.GetItems(query);
+
+                // Load the SharePoint list
+                clientContext.Load(list);
+                clientContext.Load(items);
+                clientContext.ExecuteQuery();
+
+                // Loop through the list to go through all the items which are newly added
+                foreach (ListItem item in items)
+                    if (item["IsProcessed"].ToString() != "Yes")
+                    {
+                        string name = item["Title"].ToString();
+                        string notificationTitle = item["NotificationTitle"].ToString();
+                        string notificationMessage = item["Message"].ToString();
+                        string category = item["Category"].ToString();
+                        string actionURL = ((FieldUrlValue)item["URL"]).Url;
+
+                        // Send an HTTP request to create AzME campaign
+                        int campaignId = CreateAzMECampaign
+                            (name, notificationTitle, notificationMessage, category, actionURL).Result;
+
+                        if (campaignId != -1)
+                        {
+                            // If creating campaign is successful then set this to true
+                            item["IsProcessed"] = "Yes";
+
+                            // Now try to activate the campaign also
+                            await ActivateAzMECampaign(campaignId);
+                        }
+                        else
+                        {
+                            item["IsProcessed"] = "Error";
+                        }
+                        item.Update();
+                    }
+                clientContext.ExecuteQuery();
+            }
+        }
+        
+        static async Task<int> CreateAzMECampaign(string campaignName, string notificationTitle, 
+            string notificationMessage, string notificationCategory, string actionURL)
+        {
+            string createURIFragment = "/reach/1/create";
+
+            using (var client = new HttpClient())
+            {
+                // Add Authorization Header
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", CreateAuthZHeader());
+                
+                // Create the payload to send the content
+                // Reference -> https://msdn.microsoft.com/en-us/library/dn913749.aspx
+                string data =
+                    @"{""name"":""" + campaignName + @"""," + 
+                    @"""type"":""only_notif""," + 
+                    @"""deliveryTime"":""any""," + 
+                    @"""notificationTitle"":""" + notificationTitle + 
+                    @""",""notificationMessage"":""" + notificationMessage + 
+                    @""",""actionUrl"":""" + actionURL + @"""}";
+                
+                var content = new FormUrlEncodedContent(new[] 
+                {
+                    new KeyValuePair<string, string>("kind", "announcement"),
+                    new KeyValuePair<string, string>("data", data),
+                });
+
+                // Send the POST request
+                var response = await client.PostAsync(url + createURIFragment, content);
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                int campaignId = -1;
+                if (response.StatusCode.ToString() == "OK")
+                {
+                    // This is the campaign id
+                    campaignId = Convert.ToInt32(responseString);
+                    Console.WriteLine("Campaign successfully created with id {0}", campaignId);
+                }
+                else
+                {
+                    Console.WriteLine("Campaign creation failed with error - '{0}'", responseString);
+                }
+                return campaignId;
+            }
+        }
+        
+        static async Task<bool> ActivateAzMECampaign(int campaignId)
+        {
+            string activateUriFragment = "/reach/1/activate";
+            using (var client = new HttpClient())
+            {
+                // Add Authorization Header
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", CreateAuthZHeader());
+
+                var content = new FormUrlEncodedContent(new[] 
+                {
+                    new KeyValuePair<string, string>("kind", "announcement"),
+                    new KeyValuePair<string, string>("id", campaignId.ToString()),
+                });
+
+                // Send the POST request
+                var response = await client.PostAsync(url + activateUriFragment, content);
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                if (response.StatusCode.ToString() == "OK")
+                {
+                    Console.WriteLine("Campaign successfully activated");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Campaign activation failed with error - '{0}'", responseString);
+                    return false;
+                }
+            }
+        }
+
+        static string CreateAuthZHeader()
+        {
+            string BasicAuthzHeaderString = "Basic " + EncodeTo64(ApplicationId + ":" + ApiKey);
+            return BasicAuthzHeaderString;
+        }
+
+        static public string EncodeTo64(string toEncode)
+        {
+            byte[] toEncodeAsBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
+            string returnValue = System.Convert.ToBase64String(toEncodeAsBytes);
+            return returnValue;
+        }
+    }
+}
