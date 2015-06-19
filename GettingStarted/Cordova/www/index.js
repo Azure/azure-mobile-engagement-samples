04028cb4dbd5af6e3ba63b82ee3bb32dea4994e4
Olivier Goguel <o@goguel.com>
2015-06-19 19:49:52 +0200

    update sample code style

--- GettingStarted/Cordova/www/index.js
+++ GettingStarted/Cordova/www/index.js
@@ -7,78 +7,77 @@ var app = angular.module('sampleApp', []);
 
 app.controller('appController', function($scope) {
 
-	$scope.ready  = false;
-	$scope.currentActivity=null;
+    $scope.ready  = false;
+    $scope.currentActivity=null;
 
-	$scope.onReady = function() {
-	
-		console.log("device ready");
-		$scope.ready = true;
-		$scope.cordova = cordova;
-		$scope.startActivity("home",{newbie:true});
-		AzureEngagement.getStatus(function(_status) {
-			$scope.status = _status;
-			$scope.$apply();
-		});
-		$scope.$apply();
-		AzureEngagement.onOpenURL(function(_url) {
-			$scope.alert = "Got push URL "+_url;
-			$scope.$apply();
-			console.log(alert);
-			$('#sample-alert').modal();
-		});
-	};
-	
-	$scope.onError = function(_error) {
-		console.error(_error);
-	};
+    $scope.onReady = function() {
 
+        console.log('device ready');
+        $scope.ready = true;
+        $scope.cordova = cordova;
+        $scope.startActivity('home',{newbie:true});
+        AzureEngagement.getStatus(function(_status) {
+            $scope.status = _status;
+            $scope.$apply();
+        });
+        $scope.$apply();
+        AzureEngagement.onOpenURL(function(_url) {
+            $scope.alert = 'Got push URL '+_url;
+            $scope.$apply();
+            console.log($scope.alert);
+            $('#sample-alert').modal();
+        });
+    };
 
-	$scope.startActivity = function(_activityname,_param) {
-		console.log('startActivity:'+_activityname);
-		$scope.currentActivity = _activityname;
-		AzureEngagement.startActivity(_activityname,_param,undefined,$scope.onError );
-	};
+    $scope.onError = function(_error) {
+        console.error(_error);
+    };
 
-	$scope.endActivity = function() {
-		console.log('endActivity:'+$scope.currentActivity);
-		$scope.currentActivity = null;
-		AzureEngagement.endActivity(undefined,$scope.onError );
-	};
+    $scope.startActivity = function(_activityname,_param) {
+        console.log('startActivity:'+_activityname);
+        $scope.currentActivity = _activityname;
+        AzureEngagement.startActivity(_activityname,_param,undefined,$scope.onError );
+    };
 
-	$scope.sendAppInfo = function(_appInfos) {
-		console.log('sendAppInfo:'+JSON.stringify(_appInfos));
-		AzureEngagement.sendAppInfo(_appInfos,undefined,$scope.onError);
-	};
+    $scope.endActivity = function() {
+        console.log('endActivity:'+$scope.currentActivity);
+        $scope.currentActivity = null;
+        AzureEngagement.endActivity(undefined,$scope.onError );
+    };
 
-	$scope.sendEvent = function(_eventName,_extraInfos) {
-		console.log('sendEvent:'+_eventName);
-		AzureEngagement.sendEvent(_eventName,_extraInfos,undefined,$scope.onError);
-	};
+    $scope.sendAppInfo = function(_appInfos) {
+        console.log('sendAppInfo:'+JSON.stringify(_appInfos));
+        AzureEngagement.sendAppInfo(_appInfos,undefined,$scope.onError);
+    };
 
-	$scope.startJob = function(_jobName,_extraInfos) {
-		console.log('startJob:'+_jobName);
-		AzureEngagement.startJob(_jobName,_extraInfos,undefined,$scope.onError);
-	};
+    $scope.sendEvent = function(_eventName,_extraInfos) {
+        console.log('sendEvent:'+_eventName);
+        AzureEngagement.sendEvent(_eventName,_extraInfos,undefined,$scope.onError);
+    };
 
-	$scope.endJob = function(_jobName) {
-		console.log('endJob:'+_jobName);
-		AzureEngagement.endJob(_jobName,undefined,$scope.onError);
-	};
+    $scope.startJob = function(_jobName,_extraInfos) {
+        console.log('startJob:'+_jobName);
+        AzureEngagement.startJob(_jobName,_extraInfos,undefined,$scope.onError);
+    };
 
-	$scope.registerForPushNotification = function() {
-		console.log('registerForPushNotification');;
-		AzureEngagement.registerForPushNotification(undefined,$scope.onError);
-	};
+    $scope.endJob = function(_jobName) {
+        console.log('endJob:'+_jobName);
+        AzureEngagement.endJob(_jobName,undefined,$scope.onError);
+    };
 
-	$scope.runTests = function() {
-		window.location = "cdvtests/index.html";
-	};
+    $scope.registerForPushNotification = function() {
+        console.log('registerForPushNotification');
+        AzureEngagement.registerForPushNotification(undefined,$scope.onError);
+    };
 
-	// DEBUG 
-	if (typeof cordova=="undefined")
-		$scope.ready=true;
-	else
-		document.addEventListener('deviceready', $scope.onReady, false);
-	
+    $scope.runTests = function() {
+        window.location = 'cdvtests/index.html';
+    };
+
+    // DEBUG
+    if (typeof cordova=='undefined')
+        $scope.ready=true;
+    else
+        document.addEventListener('deviceready', $scope.onReady, false);
+
 });
